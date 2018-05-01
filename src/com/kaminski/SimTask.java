2c7864e5dbe44b7eaf682f85deaa0186ceb6f778
unknown <michalkaminski37@gmail.com>
2018-05-01 19:14:55 +0200

    Sprawozdanie 3

---
+++ src/com/kaminski/SimTask.java
@@ -0,0 +1,11 @@
+package com.kaminski;
+
+import java.util.TimerTask;
+
+public class SimTask extends TimerTask {
+    
+    @Override
+    public void run() {
+        // TODO: implement
+    }
+}
---
+++ src/com/kaminski/SpringApplet.java
@@ -0,0 +1,19 @@
+package com.kaminski;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SpringApplet extends JApplet {
+
+    @Override
+    public void init() {
+        super.init();
+        // TODO: implement
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        // TODO: implement
+    }
+}
